5b949eec66dd80a810924867cdc5be4b8291c528
jarkkokilpe <jarkko.kilpelainen@protonmail.com>
2025-04-15 21:49:58 +0300

    Fix url+credentials to solution

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -3,19 +3,21 @@
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     // Visit the base URL or the Sign In page directly
-    cy.visit('https://react-redux.realworld.io/'); // Replace '/' with the actual URL path if needed
+    cy.visit('https://conduit.mate.academy/'); // Replace '/' with the actual URL path if needed
 
     // Click on the "Sign In" button in the navigation
-    cy.get('a.nav-link[href="#login"]').click();
+    cy.get('a.nav-link[href="/user/login"]').click();
 
     // Fill in the email and password fields
-    cy.get('input[placeholder="Email"]').type('testuser@example.com');
-    cy.get('input[placeholder="Password"]').type('password123');
+    cy.get('input[placeholder="Email"]')
+      .type('jarkko.kilpelainen@protonmail.com');
+    cy.get('input[placeholder="Password"]')
+      .type('jarkko.kilpelainen@protonmail.com');
 
     // Click the "Sign in" button
     cy.get('button[type="submit"]').click();
 
     // Assert that the username appears in the navigation menu
-    cy.get('.nav-item .nav-link').contains('testuser').should('be.visible'); // Replace 'testuser' with the expected username
+    cy.get('.nav-item .nav-link').contains('jarkkokilpe').should('be.visible'); // Replace 'testuser' with the expected username
   });
 });
